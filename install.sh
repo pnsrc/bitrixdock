bf2c4aece66189fb6422214fbc2bbe27093f4665
Alexey <pnsrc@narod.ru>
2024-06-06 12:50:46 +0600

    небольшие правки

--- install.sh
+++ install.sh
@@ -1,19 +1,20 @@
 #!/bin/sh
 set -e
-
+echo 'IF script stopped, just use sudo -s, and run again'
+echo '=============================================='
 echo "Create folder struct"
 mkdir -p /var/www/bitrix && \
 cd /var/www && \
 rm -f /var/www/bitrix/bitrixsetup.php && \
 curl -fsSL https://www.1c-bitrix.ru/download/scripts/bitrixsetup.php -o /var/www/bitrix/bitrixsetup.php && \
 rm -rf /var/www/bitrixdock && \
-git clone --depth=1 https://github.com/bitrixdock/bitrixdock.git && \
+git clone --depth=1 https://github.com/pnsrc/bitrixdock.git && \
 chmod -R 775 /var/www/bitrix && chown -R root:www-data /var/www/bitrix && \
-
+echo '=============================================='
 echo "Config"
 cp -f /var/www/bitrixdock/.env_template /var/www/bitrixdock/.env
 sed -i 's/SITE_PATH=.\/www/SITE_PATH=\/var\/www\/bitrix/' /var/www/bitrixdock/.env
-
-echo "Run"
+echo '=============================================='
+echo "Running bitrix"
 docker compose -p bitrixdock down
 docker compose -f /var/www/bitrixdock/docker-compose.yml -p bitrixdock up -d
